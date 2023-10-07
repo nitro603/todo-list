20387f785c45113061f179216dd49feb621e6c6e
nitro603 <tavaracristian93@gmail.com>
2023-10-07 10:52:54 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import TodoForm from "./Components/TodoForm";
-import TodoList from "./Components/TodoList";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <TodoList/>
+    <div className="bg-black">
+      hello
     </div>
   );
 }
+
 export default App;
